563ca26a675d25606fad87ddfe171898bf882e41
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-09-27 15:39:28 -0500

    Create addTwoNumbers function

--- challenges.js
+++ challenges.js
@@ -57,7 +57,9 @@ addTwoNumbers(0, 0) //=> 0
 addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
-
+function addTwoNumbers (num1, num2) {
+  return (isNaN(num1) || isNaN(num2)) ? NaN : num1 + num2;
+  }
 
 
 
